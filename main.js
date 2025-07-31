5eea6c211047eb433a0c340634bb6c80a72c71dc
Abigail <abigailf03@hotmail.ca>
2025-07-31 14:49:22 -0500

    added preload script

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } = require("electron/main");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 600,
     height: 700,
-    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
-  win.setMenuBarVisibility(false);
 
   win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
   createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
